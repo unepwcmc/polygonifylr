45a72e7e89b45827153a81f442d94209044780f4
André Neves <Andre.Neves@unep-wcmc.org>
2011-03-28 16:57:59 +0100

    Init repo, empty.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Basemap::Application.config.secret_token = '1624689f32740c2cc2c4031dcc0aa5372ea86b55ad30c63ea5b9e7958e2e06e48c5a4a30007c4f22ece121103692b9150091c85f7fabe4c90cfadf1e46b4ed9e'
