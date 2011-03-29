1fab8292fe906b2bf69fce42bef91c5f3a566473
André Neves <Andre.Neves@unep-wcmc.org>
2011-03-29 18:05:54 +0100

    Updated main page to index.Removed link to delete.
    (Does not delete? Odd.)

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ Basemap::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "polygons#new"
+  root :to => "polygons#index"
 
   # See how all your routes lay out with "rake routes"
 
